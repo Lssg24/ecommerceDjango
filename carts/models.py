35bf61933e3b720e663112479ca45d6d99547c32
luxitomario <lssg24@hotmail.com>
2023-09-14 17:43:24 -0300

    ecommerce v2

---
+++ carts/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from store.models import Product
+from django.db import models
+
+# Create your models here.
+
+
+class Cart(models.Model):
+    cart_id = models.CharField(max_length=250, blank=True)
+    date_added = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.cart_id
+
+
+class CartItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    is_activate = models.BooleanField(default=True)
+
+    def __unicode__(self):
+        return self.product
